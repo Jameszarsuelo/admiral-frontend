4444b569f213d17c080ef963c1dc642abbf6317e
Charles-indigo21 <charles.verdadero@indigo21.com>
2025-11-20 00:11:30 +0800

    Create OutcomeSchema.ts

---
+++ src/types/OutcomeSchema.ts
@@ -0,0 +1,15 @@
+import z from "zod"
+
+export const OutcomeSchema = z.object({
+    id: z.number().optional(),
+    status: z.boolean().optional(),
+    outcome_code: z.string().nonempty("Outcome Code is required"),
+    classification: z.string().nonempty("Classification is required"),
+    queue: z.string().nonempty("Queue is required"),
+    description: z.string().nonempty("Description is required"),
+    created_at: z.string().optional(),
+    updated_at: z.string().optional(),
+});
+
+export type IOutcomeForm = z.infer<typeof OutcomeSchema>
+export type IOutcomeHeaders = z.infer<typeof OutcomeSchema>
